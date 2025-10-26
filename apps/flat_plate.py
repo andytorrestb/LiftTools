64826ea29f94a8cdc607fa381ca1c654313419f0
andytorrestb <andytorrestb@gmail.com>
2025-10-26 08:03:45 -0400

    WIP: Added WA for a flapped plate

--- apps/flat_plate.py
+++ apps/flat_plate.py
@@ -10,11 +10,12 @@ import models.ThinAirfoilTheory as tat
 import geometry.FlatPlate as fp
 
 if __name__ == "__main__":
-    # Load flat plate and plot at various angles of attack
+    # Step 1) Load flat plate and plot at various angles of attack
     # Provides a simple sanity check of geometry rotation.
     chord = 1.0  # chord length
     alpha = np.linspace(0, math.pi/2.0, 5)  # angles of attack in radians
     flat_plate_geom = fp.FlatPlate(chord=chord)
+
 
     plt.figure(figsize=(8, 4))
     for a in alpha:
@@ -33,19 +34,40 @@ if __name__ == "__main__":
          plt.grid(True)
     plt.savefig(f"flat_plate_rotations.png")
 
-    # Run Thin Airfoil Theory model on flat plate using same geometry and alpha values.
+    # Step 2) Run Thin Airfoil Theory model on flat plate using same geometry and alpha values.
     for a in alpha:
-          flat_plate_geom.alpha['value'] = a
-          flat_plate_geom.compute_endpoint_values()
+        flat_plate_geom.alpha['value'] = a
+        flat_plate_geom.compute_endpoint_values()
 
-          tat_model = tat.ThinAirfoilTheory(
-               geometry=flat_plate_geom,
-               alpha_rad=a,
-               panels=1,
-          )
+        tat_model = tat.ThinAirfoilTheory(
+                geometry=flat_plate_geom,
+                alpha_rad=a,
+                panels=1,
+        )
 
-          tat_model.set_z_expr(foil_type="plate")
-          results = tat_model.solve()
-          cl = results['cl']
-          print(f"TAT: Alpha = {a} deg, CL = {cl:.4f}")
+        tat_model.set_z_expr(foil_type="plate")
+        results = tat_model.solve()
+        cl = results['cl']
+        print(f"TAT: Alpha = {a} deg, CL = {cl:.4f}")
 
+     # Step 3) Run Weisinger's Approximation model on flat plate with a flap.
+    deflection_rad = math.radians(5)  # 15 degree flap deflection
+
+    for a in alpha:
+        flat_plate_geom.alpha['value'] = a
+        flat_plate_geom.compute_endpoint_values()
+
+        wa_model = wa.WeisingersApprox(
+        geometry=flat_plate_geom,
+        alpha_rad=a,
+        panels=2,
+    )
+
+    wa_model.set_flap(deflection_rad=deflection_rad, length_le=0.7)
+    wa_model.set_flow_conditions(
+        U_inf=10.0,  # m/s
+        rho_inf=1.225,  # kg/m^3
+    )
+    results = wa_model.solve_plate()
+    cl = results['cl']
+    print(f"Weisinger's Approx: Alpha = {a} deg, CL = {cl:.4f}")
--- src/models/WeisingersApprox.py
+++ src/models/WeisingersApprox.py
@@ -1,1 +1,70 @@
+import math
+import numpy as np
+
 from models.AirFoilModel import AirfoilModel
+
+class WeisingersApprox(AirfoilModel):
+
+    def solve(self) -> dict:
+        # Placeholder for Weisinger's Approximation solution method
+        pass
+
+    def set_flap(self, deflection_rad: float, length_le: float) -> None:
+        # Placeholder for setting flap deflection specific to Weisinger's Approximation
+        self.geometry.flap_deflection_rad = float(deflection_rad)
+        self.geometry.flap_length_le = float(length_le)
+        pass
+
+    def solve_plate(self) -> None:
+        # Placeholder for setting control points specific to Weisinger's Approximation
+
+        c = self.geometry.chord['value']
+        p = self.geometry.pivot['value']
+        alpha = self.geometry.alpha['value']
+
+        delta = self.geometry.flap_deflection_rad
+        l_le = self.geometry.flap_length_le
+
+        # Calculate main and flap chord lengths
+        main_c = l_le * c
+        flap_c = (1 - l_le) * c
+
+        # Calulculate Vortex source locations.
+        G1_x = 0.25 * main_c
+        G2_x = main_c + 0.25 * flap_c
+
+        # Calculate location of flow tangency conditions.
+        V1_x = 0.75 * main_c
+        V2_x = main_c + 0.75 * flap_c
+
+        # Calculate distances from vortices to control points.
+        r11 = G1_x - V1_x
+        r12 = G1_x - V2_x
+        r21 = G2_x - V1_x
+        r22 = G2_x - V2_x
+
+        # Define influence coefficient matrix
+        A = np.array([[1, 1],
+                      [math.sin(self.geometry.flap_deflection_rad), -1]])
+        
+        # Define right-hand side vector using flow tangency conditions.
+        b = np.array([
+            2 * math.pi * alpha,
+            2 * math.pi * (alpha - delta)
+        ])  
+
+        G = np.linalg.solve(A, b)
+
+        L = self.rho_inf * self.U_inf * (G[0] + G[1])
+        cl = L / (0.5 * self.rho_inf * self.U_inf**2 * c)
+        cd = 0.0  # Placeholder for drag coefficient calculation
+        cm_cg = 0.0  # Placeholder for moment coefficient calculation
+        
+        results = {
+            'L': L,
+            'cl': cl,
+            'cd': cd,
+            'cm_cg': cm_cg
+        }
+
+        return results
